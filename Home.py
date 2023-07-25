6531c79b4112746c10920470bedd22a7d10015c6
Georgio Feghali <75293296+GeorgioFe@users.noreply.github.com>
2023-07-24 23:41:41 -0700

    Modifed branding and main page config.

--- Home.py
+++ Home.py
@@ -1,15 +1,17 @@
 import streamlit as st
 from streamlit_card import card
 from streamlit_extras.switch_page_button import switch_page
+from PIL import Image
 
+favicon = Image.open("admin/branding/logos/favicon-32x32.png")
 st.set_page_config(
-    page_title="Solvr.ai",
-    page_icon="",
+    page_title="solvr.ai",
+    page_icon=favicon,
     layout="wide",
-    initial_sidebar_state="auto"
+    initial_sidebar_state="collapsed"
 )
 
-st.markdown("<h1 style='text-align: center;'>Solvr.<font color='#FF4B4B'>ai</font></h1>", unsafe_allow_html=True)
+st.markdown("<h1 style='font-size:60px'><font color='#FF4B4B'>so\\vr.ai</font></h1>", unsafe_allow_html=True)
 
 col1, col2, col3 = st.columns(3)
 
@@ -24,9 +26,7 @@ with col1:
           "height": "200px",
           "border-radius": "10px",
           "box-shadow": "0 0 10px rgba(0,0,0,0.5)",
-          "background-color": "#3e3e42",
-          "border-color": "#FF4B4B",
-          "border-width": "20px"
+          "background-color": "#3e3e42"
       }
     },
     on_click=lambda: switch_page("Chat Assistant")
@@ -34,7 +34,7 @@ with col1:
 
   pdf_summarizer = card(
     title="🖼️",
-    text="Image Generation",
+    text="Image Generator",
     key=4,
     styles={
       "card": {
