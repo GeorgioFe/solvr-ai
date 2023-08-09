7ca647d9e62781fc16cfb658cc5fa0a35a0fb8f0
Georgio Feghali <75293296+GeorgioFe@users.noreply.github.com>
2023-08-08 20:56:12 -0700

    Image generator prototype done.

--- pages/4-Image_Generation.py
+++ pages/4-Image_Generation.py
@@ -2,6 +2,9 @@
 import streamlit as st
 from PIL import Image
 
+# Logic Dependencies
+import openai
+
 # Page Configuration.
 favicon = Image.open("./admin/branding/logos/favicon-32x32.png")
 st.set_page_config(
@@ -12,3 +15,21 @@ st.set_page_config(
 )
 
 st.markdown("<h1 style='text-align: center; vertical-align: middle;'>Stay Tuned! It's coming soon 👀 🏗️.</h1>", unsafe_allow_html=True)
+
+# ## Testing DALL-E
+# def create_image(prompt):
+#     openai.api_key = st.secrets['openai_api_key']
+#     response = openai.Image.create(
+#         prompt=prompt,
+#         n=1,
+#         size="1024x1024"
+#     )
+
+#     img_url = response['data'][0]['url']
+
+#     return img_url
+
+# print(create_image("A painting of a cat sitting on a chair."))
+
+
+
